9c3b4323050a0abe936b9e0cfb25b6d94eabf801
Michel Miranda <mmjcr05@gmail.com>
2025-10-29 02:14:14 -0300

    criando o controller usuario

--- backend/src/controllers/controller_usuario.js
+++ backend/src/controllers/controller_usuario.js
@@ -1,15 +1,18 @@
 db = require("../database/db")
+const bcrypt = require("bcrypt");
+const salt = bcrypt.genSaltSync(10);
+
 
 
 
 exports.cadastrarUsuario = (req,res) => {
   const nome_usuario = req.body.nome_usuario;
   const email_usuario = req.body.email_usuario;
-  const senha_usuario = req.body.senha_usuario;
+  const hashSenha =  bcrypt.hashSync(req.body.senha_usuario, salt); 
   const admin_usuario = req.body.admin_usuario;
 
   db.query('INSERT INTO `portal_noticias`.`usuarios` (nome_usuario,email_usuario,senha_usuario,admin_usuario) VALUES(?,?,?,?)',
-  [nome_usuario, email_usuario, senha_usuario, admin_usuario], (err, results) =>{
+  [nome_usuario, email_usuario, hashSenha, admin_usuario], (err, results) =>{
     if (err) {
        console.error('Erro ao criar usuario:', err);
       return res.status(500).json({ error: 'Erro ao criar usuario' });
@@ -17,6 +20,7 @@ exports.cadastrarUsuario = (req,res) => {
      res.redirect("/usuarios/editar")
   });
 }
+
 
 
 exports.atualizarUsuario = (req,res) => {

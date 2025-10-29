bdde348fea4aa810d483919bc07be9ec616e8d20
Michel Miranda <mmjcr05@gmail.com>
2025-10-29 02:13:58 -0300

    atualizando o nome da rota

---
+++ backend/src/routes/routesArtigos.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const router = express.Router();
+const controller_artigo = require('../controllers/controller_artigo.js');
+const uploadArquivo = controller_artigo.uploadArquivo
+
+
+
+router.get('/editar', controller_artigo.listarArtigos);
+router.get('/:id', controller_artigo.buscar_artigo);
+router.post('/cadastrar_artigo', uploadArquivo.single("imagem_destaque_artigo"), (req, res) => {
+   console.log('Arquivo recebido:', req.file); // Verifique se o arquivo está sendo recebido
+  const action = req.body.action;
+
+  if (action === 'incluir') {
+    controller_artigo.criar_artigo(req, res);
+  } else if (action === 'atualizar') {
+    controller_artigo.atualizar_artigo(req, res);
+  } else {
+    res.status(400).json({ error: 'Ação inválida' });
+  }
+ });
+router.post('/deletar_artigo', controller_artigo.deletar_artigo);
+
+
+module.exports = router;

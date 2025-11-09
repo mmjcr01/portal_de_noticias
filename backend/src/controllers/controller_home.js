7c1d324768df4303cc32f28ef8f10db989d779dc
Michel Miranda <mmjcr05@gmail.com>
2025-11-09 12:53:17 -0300

    criando a função mais lido da semana

--- backend/src/controllers/controller_home.js
+++ backend/src/controllers/controller_home.js
@@ -33,6 +33,39 @@ exports.listarArtigosDestaque = async (req, res) => {
     throw err; // Lança o erro para ser tratado na função chamadora
   }
 };
+exports.maisLidosSemana = async () => {
+  const sql = `
+    SELECT 
+        v.id_artigo,
+        v.titulo_artigo,
+        v.resumo_artigo,
+        v.conteudo_artigo,
+        v.imagem_destaque_artigo,
+        v.alt_imagem,
+        v.destaque,
+        v.id_categoria,
+        v.nome_categoria,
+        v.id_autor,
+        v.nome_autor,
+        COUNT(s.id_artigo) AS total_acessos
+    FROM vw_artigos v
+    JOIN sessao_usuarios s ON s.id_artigo = v.id_artigo
+    WHERE s.data_hora_acesso >= DATE_SUB(NOW(), INTERVAL 7 DAY)
+    GROUP BY v.id_artigo
+    ORDER BY total_acessos DESC
+    LIMIT 4;
+  `;
+
+  return new Promise((resolve, reject) => {
+    db.query(sql, (err, results) => {
+      if (err) {
+        console.error("Erro ao buscar os mais lidos:", err);
+        return reject(err);
+      }
+      resolve(results);
+    });
+  });
+};
 
 
 exports.listarArtigos = async (req, res) => {
@@ -52,8 +85,9 @@ exports.listarArtigos = async (req, res) => {
     // Buscar artigos com destaque
     const artigos_destaque = await exports.listarArtigosDestaque(req, res);
     const categorias = await exports.buscarCategorias();
+    const artigosMaisLidos = await exports.maisLidosSemana();
     // Renderizar a view com os dados
-    res.render("index", { login: req.session.user|| null, artigos, artigos_destaque, categorias, base_imagem });
+    res.render("index", { login: req.session.user|| null, artigos, artigos_destaque, artigosMaisLidos, categorias, base_imagem });
   } catch (err) {
     console.error('Erro ao listar artigos ou autores:', err);
     res.status(500).json({ error: 'Erro ao listar artigos ou autores' });
@@ -74,7 +108,7 @@ exports.buscar_artigo_por_categoria = async (req, res) => {
     resolve(results);
   }); 
  });
-   // res.render("teste" , {artigos})
+   
   }  catch(err) {
     console.error('Erro ao listar artigos:', err);
     res.status(500).json({ error: 'Erro ao listar artigos' });
